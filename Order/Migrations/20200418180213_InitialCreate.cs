ea5b8f890119aef64280a3681a3ee6dfe499cb08
cihan aşan <cihanasan@hotmail.com>
2020-04-19 23:33:15 +0300

    Add project files.

---
+++ Order/Migrations/20200418180213_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Order.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    ReceivedDate = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ Order/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Order.Models
+{
+    public class Order
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string ReceivedDate { get; set; }
+    }
+}
